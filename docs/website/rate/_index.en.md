b1e128a813091c8fcbe874251685b89e11ccb4e2
t-katsumura <t-katsumura@nri.co.jp>
2025-06-23 10:12:38 +0900

    add rate limit docs
    Signed-off-by: t-katsumura <t-katsumura@nri.co.jp>

---
+++ docs/website/rate/_index.en.md
@@ -0,0 +1,13 @@
+---
+title: "Rate Limiting"
+linkTitle: "Rate Limiting"
+type: docs
+weight: 1
+categories: []
+tags: []
+description: ""
+---
+
+{{% alert title="Info" color="info" %}}
+This page has not been translated yet.
+{{% /alert %}}
---
+++ docs/website/rate/ex_slidingwindow/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"math/rand/v2"
+	"net/http"
+	"time"
+
+	"github.com/aileron-projects/go/ztime/zrate"
+)
+
+func main() {
+	limit := 10
+	limiter := zrate.NewSlidingWindowLimiter(limit)
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		token := limiter.AllowNow()
+		defer token.Release()
+		if token.OK() {
+			time.Sleep(time.Duration(rand.Int64N(100)) * time.Millisecond)
+			w.WriteHeader(http.StatusOK)
+			_, _ = w.Write([]byte("ok"))
+		} else {
+			w.WriteHeader(http.StatusTooManyRequests)
+			_, _ = w.Write([]byte("too many requests"))
+		}
+	})
+
+	log.Println("server listens on localhost:8080")
+	svr := &http.Server{
+		Addr:        ":8080",
+		Handler:     handler,
+		ReadTimeout: 10 * time.Second,
+	}
+	if err := svr.ListenAndServe(); err != nil {
+		panic(err)
+	}
+}
